dda06f655c248628acedcd6e13bfc2dd3dece1d5
maguranon <maguranon@yahoo.com>
2025-05-05 21:58:53 +0200

    Update and rename 2.5 Manipolazione di vettori (stipendi).py to 2.4 Analisi della Frequenza Cardiaca con NumPy.py

---
+++ 2.4 Analisi della Frequenza Cardiaca con NumPy.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numpy as np
+
+a = np.array([68, 65, 77, 110, 160, 161, 162, 161, 160, 161, 162, 163, 164, 163, 162, 100, 90, 97, 72, 60, 70])
+min = np.min(a)
+max = np.max(a)
+sopra_120 = a > 120
+media = np.mean(sopra_120) * 100
