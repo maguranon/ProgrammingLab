48dac7565d7b75f8b4073aeae3064a5739ee8cd3
maguranon <maguranon@yahoo.com>
2025-07-21 16:06:56 +0200

    Update 1. Trasformare cicli in list comprehension.py

--- 1. Trasformare cicli in list comprehension.py
+++ 1. Trasformare cicli in list comprehension.py
@@ -1,21 +1,21 @@
 quadrati = []
 for i in range(10):
-    quadrati.append(1**2)
+    quadrati.append(i**2)
 
-quadrati = [1**2 for i in range(10)]
+quadrati = [i**2 for i in range(10)]
 
 pari = []
 for i in range(10):
    if i % 2 == 0:
        pari.append(i)
 
-pari = [1 for i in range(10) if i % 2 == 0]
+pari = [i for i in range(10) if i % 2 == 0]
 
 pari_e_quadrati = []
 
 for i in range(100):
    if i % 2 == 0:
-       pari_e_quadrati.append(1**2)
+       pari_e_quadrati.append(i**2)
 
-pari_e_quadrati = [1**2 for i in range(100) if i % 2 == 0]
+pari_e_quadrati = [i**2 for i in range(100) if i % 2 == 0]
 
