e9126b1b45635d413da94e24332b1c547e910fff
maguranon <maguranon@yahoo.com>
2025-07-21 16:10:16 +0200

    Update 3. Operazioni su array.py

--- 3. Operazioni su array.py
+++ 3. Operazioni su array.py
@@ -1,12 +1,12 @@
 import numpy as np
 import random
 
-a = np.random.choice(array, size = 10) * (1 - 1e-10) + 1e-10
+a = np.random.randint(1, 10, size = 10)
 b = a [1:4]
 c = np.flip(a)
 divisione = a / c
 
-a = [random.randiant(1, 10) for _ in range(10)]
+a = [random.randint(1, 10) for _ in range(10)]
 b = a[1:4]
 c = a[::-1]
-divisione = [x/y for x, y in zip(a, c)]
+divisione = [x/y if y != 0 else np.nan for x, y in zip(a, c)]
