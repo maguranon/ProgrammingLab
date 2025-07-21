3a9f3fd64c57b6c84d9345de9f7eed86b9be3425
maguranon <maguranon@yahoo.com>
2025-07-21 18:16:53 +0200

    Update 2.2. Operazioni su array.py

--- 2.2. Operazioni su array.py
+++ 2.2. Operazioni su array.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-a = np.random.radint(1, 100, size=(5, 4))
+a = np.random.randint(1, 100, size=(5, 4))
+print("Matrice originale 'a':\n", a)
+
 b = a[[1, 3]]
+print("\nSotto-matrice 'b' (righe 1 e 3):\n", b)
+
 c = a[2]
+print("\nTerza riga 'c':\n", c)
+
 diviso = a / c
+print("\nMatrice 'a' divisa per riga 'c' (broadcasting):\n", diviso)
