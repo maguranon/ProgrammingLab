d976a18a6ae15110d5c9cfd2380cb289cf4226d8
maguranon <maguranon@yahoo.com>
2025-07-21 18:15:23 +0200

    Update 2.1 Indicizzazione booleana.py

--- 2.1 Indicizzazione booleana.py
+++ 2.1 Indicizzazione booleana.py
@@ -1,4 +1,9 @@
 import numpy as np
 a = np.array([2, 3, 5, 7, 11, 13, 17, 19])
+print("Array originale:", a)
+
 maggiori_di_10 = a[a > 10]
+print("Elementi > 10:", maggiori_di_10)
+
 pari = a[a % 2 == 0]
+print("Numeri pari:", pari)
