1348feb8ca75b7420261b2f8adcc6da8cdb6a28a
maguranon <maguranon@yahoo.com>
2025-07-21 18:28:12 +0200

    Update 2.7 Analisi di una matrice.py

--- 2.7 Analisi di una matrice.py
+++ 2.7 Analisi di una matrice.py
@@ -6,6 +6,17 @@ survey_matrix = np.array([
     [29, 47000, 11],
     [35, 58000, 13]
 ])
+
+print("Matrice completa del sondaggio:")
+print(survey_matrix)
+
 istruzione_12_plus = survey_matrix[survey_matrix[:,2] >= 12]
+print("\nSoggetti con 12+ anni di istruzione:")
+print(istruzione_12_plus)
+
 redditi_12_plus = survey_matrix[survey_matrix[:, 2] >= 12][:, 1]
+print("\nRedditi dei soggetti con 12+ anni di istruzione:")
+print(redditi_12_plus)
+
 media_redditi = np.mean(survey_matrix[survey_matrix[:, 2] >= 12][:, 1])
+print("\nMedia redditi per 12+ anni di istruzione: €", round(media_redditi, 2))
