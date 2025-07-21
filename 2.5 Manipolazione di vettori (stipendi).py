69f04b4349afa37aefe5c13b7c288d1624b3cae8
maguranon <maguranon@yahoo.com>
2025-07-21 16:21:18 +0200

    Update 2.5 Manipolazione di vettori (stipendi).py

--- 2.5 Manipolazione di vettori (stipendi).py
+++ 2.5 Manipolazione di vettori (stipendi).py
@@ -2,7 +2,7 @@ import numpy as np
 stipendi = np.array([50000.0, 105250.0, 55000.0, 89000.0])
 totale_iniziale = np.sum(stipendi)
 stipendi[stipendi == 105250.0] *= 1.15
-stipendi = np.array([50000.0, 105250.0, 55000.0, 89000.0])
+stipendi = np.array([50000.0, 105250.0, 55000.0, 89000.0], dtype=object)
 stipendi[stipendi == 105250] = 105250 * 1.15
 stipendi[stipendi == 50000] = 50000 * 1.20
 
