3bda7a1bc96c57424d097cb6b713ce10d6646034
maguranon <maguranon@yahoo.com>
2025-07-21 18:18:25 +0200

    Update 2.3. Seleziona il numero più vicino a 0.5 in un array casuale.py

--- 2.3. Seleziona il numero più vicino a 0.5 in un array casuale.py
+++ 2.3. Seleziona il numero più vicino a 0.5 in un array casuale.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 a = np.random.rand(10, 3)
+print("Matrice originale 'a':\n", a)
+
 diff = np.abs(a - 0.5)
+print("\nDistanze da 0.5:\n", diff)
+
 indici = np.argmin(diff, axis=1)
+print("\nIndici dei valori più vicini a 0.5 per riga:", indici)
+
 valori_vicini = a[np.arange(10), indici]
+print("\nValori più vicini a 0.5 in ogni riga:\n", valori_vicini)
